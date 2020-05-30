73785d66f123d688ddc3795ffb38ccc6c2d06021
Teresa Cho <tcho@cca.edu>
2020-05-29 17:47:46 -0700

    imports render from react-dom and renders a test paragraph tag to the app --> success

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 class StorePicker extends React.Component {
-  
+  render() {
+    return <p>Hello!</p>
+  }
 }
+
+render(<p>HEYYYYOOOO</p>, document.querySelector('#main'));
