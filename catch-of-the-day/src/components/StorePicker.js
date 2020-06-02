91bf5bfc2a3b78e8d9c60ea7ff220b8e5b3733c5
Teresa Cho <tcho@cca.edu>
2020-06-01 18:10:52 -0700

    creates a button tag w/ type of submit

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -6,6 +6,7 @@ class StorePicker extends React.Component {
         <form className='store-selector'>
           <h2>Please Enter a Store</h2>
           <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store</button>
         </form>
     )  
   }
