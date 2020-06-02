052f69d8da7f59b9751e8504b784c6ef6d03ec54
Teresa Cho <tcho@cca.edu>
2020-06-01 18:08:51 -0700

    creates am input tag for storeName

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,13 +3,10 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <>
-        <p>Fish!</p>
         <form className='store-selector'>
-          { /* comment */ }
           <h2>Please Enter a Store</h2>
+          <input type="text" required placeholder="Store Name" />
         </form>
-      </>
     )  
   }
 }
