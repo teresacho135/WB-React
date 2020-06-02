31df096806936eff700d1ab6c8c9d425f4918f4b
Teresa Cho <tcho@cca.edu>
2020-06-01 17:56:26 -0700

    creates react fragments

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,10 +3,12 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <form className='store-selector'>
+      <>
         <p>Fish!</p>
-        <h2>Please Enter a Store</h2>
-      </form>
+        <form className='store-selector'>
+          <h2>Please Enter a Store</h2>
+        </form>
+      </>
     )  
   }
 }
