33332cdc0e93be75119c5b9df3ac964c2ce6970a
Teresa Cho <tcho@cca.edu>
2020-06-01 17:43:00 -0700

    creates a indented JSX inside of return statement

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <form className='form'>
-      <h2>Please Enter a Store</h2>
-    </form>
+    return (
+      <form className='form'>
+        <h2>Please Enter a Store</h2>
+      </form>
+    )  
   }
 }
 
