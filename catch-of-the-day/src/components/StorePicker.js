99956beb7628cad206286afd70bd506c69f5d427
Teresa Cho <tcho@cca.edu>
2020-06-01 17:46:39 -0700

    creates an error because JSX cannot parse more than one element

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,6 +3,7 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
+      <p>Fish!</p>
       <form className='form'>
         <h2>Please Enter a Store</h2>
       </form>
