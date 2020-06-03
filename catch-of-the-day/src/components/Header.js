f0c945c90f96815f5a8692e1b4d68f576dff5b49
Teresa Cho <tcho@cca.edu>
2020-06-02 23:45:00 -0700

    creates span tags of the header for styling

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,7 +4,14 @@ class Header extends React.Component{
   render() {
     return (
       <header className='top'>
-        <h1>Catch of the Day</h1>
+        <h1>
+          Catch 
+          <span className='ofThe'>
+            <span className='of'>Of</span>
+            <span className='the'>The</span>
+          </span>
+           Day
+          </h1>
           <h3 className='tagline'>
             <span>Fresh Daily</span>
           </h3>
