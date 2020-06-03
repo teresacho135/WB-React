f73fb9d2dd00facd3054d72b543d2708fc5f8d0c
Teresa Cho <tcho@cca.edu>
2020-06-02 18:34:46 -0700

    adds a div placeholder for return in App.js class

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    
+    return (
+      <div className='catch-of-the-day'>
+        
+      </div>
+    )
   }
 }
 
