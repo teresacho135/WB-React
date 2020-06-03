803a7f5e7e97a4bdf7bb7d57478c58f3c16cea49
Teresa Cho <tcho@cca.edu>
2020-06-02 18:49:33 -0700

    hard-coded App instead of storepicker

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -4,4 +4,4 @@ import StorePicker from './components/StorePicker';
 import App from './components/App';
 import "./css/style.css"
 
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
