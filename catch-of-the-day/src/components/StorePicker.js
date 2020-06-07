b51a00f35997d712ace7c5da2d569fc4d04579c6
Teresa Cho <tcho@cca.edu>
2020-06-07 15:55:20 -0700

    adds a text input created from the helper.js function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -6,7 +6,7 @@ class StorePicker extends React.Component {
     return (
         <form className='store-selector'>
           <h2>Please Enter a Store</h2>
-          <input type="text" required placeholder="Store Name" />
+          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
           <button type="submit">Visit Store</button>
         </form>
     )  
