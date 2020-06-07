e14ac0403293cc891098473ee200fda464409aa6
Teresa Cho <tcho@cca.edu>
2020-06-06 18:24:47 -0700

    imports React into router.js as well as an export default

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StorePicker from './StorePicker';
 
@@ -8,3 +9,5 @@ const Router = () => (
     </Switch>
   </BrowserRouter>
 )
+
+export default Router;
