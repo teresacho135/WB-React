4d6785cb9f2147ebb99d1fddb947f9c3d38d3827
Teresa Cho <tcho@cca.edu>
2020-06-06 17:26:46 -0700

    imports CSS styles in our index.js

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import '../css/style.css'; 
 
 class App extends React.Component {
   render() {
