11aa69ddcdf7ea69e6a87065f99b57791eb38d9f
Teresa Cho <tcho@cca.edu>
2020-06-07 00:24:58 -0700

    imports NotFound.js file

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StorePicker from './StorePicker';
 import App from './App';
+import NotFound from './NotFound';
 
 const Router = () => (
   <BrowserRouter>
