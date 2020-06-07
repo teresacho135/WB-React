d48ece9b094a066484e2cec96fb4df50a7183081
Teresa Cho <tcho@cca.edu>
2020-06-06 18:36:08 -0700

    creates a last route as catch-all for route outside of our paths will be rendered a NotFound component

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -8,6 +8,7 @@ const Router = () => (
     <Switch>
       <Route exact path='/' component={StorePicker} />
       <Route path='/store/:storeId' component={App} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 )
