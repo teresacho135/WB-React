392de46bdb6e263e58480be69f763f9428e323ca
Teresa Cho <tcho@cca.edu>
2020-06-07 17:28:12 -0700

    removes the handleclick and button --> done for testing purposes

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,10 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  handleClick() {
-    alert('heyy');
-  };
-
   render() {
     return (
         <form className='store-selector'>
           <h2>Please Enter a Store</h2>
-          <button onClick={this.handleClick}>Click Me!</button>
           <input 
             type="text" 
             required placeholder="Store Name"
