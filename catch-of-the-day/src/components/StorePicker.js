a40aa35be16441e9c68ec837658cae25cf2dbf16
Teresa Cho <tcho@cca.edu>
2020-06-07 17:21:18 -0700

    creates a button tag w/ an event listener inside of StorePicker.js

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -6,6 +6,7 @@ class StorePicker extends React.Component {
     return (
         <form className='store-selector'>
           <h2>Please Enter a Store</h2>
+          <button onClick={this.handleClick}>Click Me!</button>
           <input 
             type="text" 
             required placeholder="Store Name"
