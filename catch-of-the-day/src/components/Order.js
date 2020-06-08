00f2b3c70394ce676b4b5a539e19119078127ea4
Teresa Cho <tcho@cca.edu>
2020-06-08 14:33:06 -0700

    changes the rendered div inside of Order.js to oneliner

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 class Order extends React.Component {
   render() {
-    return (
-      <div className='Order'>
-        <p>Here is Order</p>
-      </div>
-    )
+    return <div className="order">Order!!!</div>;
   }
-};
+}
 
 export default Order;
