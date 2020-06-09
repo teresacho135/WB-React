c5aea35f584bcc8f1166b7499030cbacee349d04
Teresa Cho <tcho@cca.edu>
2020-06-09 16:36:54 -0700

    creates a form inside w/ a placeholder paragraph tag for test

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -3,9 +3,11 @@ import React from 'react';
 class AddFishForm extends React.Component {
   render() {
     return (
-    <div className="AddFishForm">
-      <h2>AddFishForm!!!</h2>
-      </div>
+      <form>
+        <p>
+          It works y'all!
+        </p>
+      </form>
     );
   }
 }
