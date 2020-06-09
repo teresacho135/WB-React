60513c7a4f1175dc37986103990d80aae21573f9
Teresa Cho <tcho@cca.edu>
2020-06-09 16:01:40 -0700

    moves the rendered JSX in inventory.js into its own lines

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Inventory extends React.Component {
   render() {
-    return <div className="inventory">Inventory!!!!</div>;
+    return (
+    <div className="inventory">
+      <h2>Inventory!!!</h2>
+      </div>
+    );
   }
 }
 
