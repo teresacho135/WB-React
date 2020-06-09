7edfafa0e5df71ed8b372d91593183cc621334b4
Teresa Cho <tcho@cca.edu>
2020-06-09 16:26:04 -0700

    creates a addFishFile.js into components

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  render() {
+    return (
+    <div className="AddFishForm">
+      <h2>AddFishForm!!!</h2>
+      </div>
+    );
+  }
+}
+
+export default AddFishForm;
