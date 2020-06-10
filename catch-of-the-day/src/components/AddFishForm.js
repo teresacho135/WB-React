348b15391314bfabdd8f7e933e3df615db1a2fe5
Teresa Cho <tcho@cca.edu>
2020-06-09 19:06:26 -0700

    completes the creation of refs of each input

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
   
   createFish = event => {
     //1. stop the form from submitting 
@@ -12,14 +17,14 @@ class AddFishForm extends React.Component {
   render() {
     return (
       <form className='fish-edit' onSubmit={this.createFish}>
-        <input name="name" ref='name' type="text" placeholder="Name" />
-        <input name="price" ref='price' type="text" placeholder="Price" />
-        <select name="status" ref='status'>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
+        <select name="status" ref={this.statusRef}>
           <option value='available'>Fresh!</option>
           <option value='unavailable'>Sold Out!</option>
         </select>
-        <textarea name="desc" ref='desc' placeholder="Desc" />
-        <input name="image" ref='image' type="text" placeholder="Image" />
+        <textarea name="desc" ref={this.descRef} placeholder="Desc" />
+        <input name="image" ref={this.imageRef} type="text" placeholder="Image" />
         <button type='submit'> + Add Fish</button>
       </form>
     );
