c25f002d90e8b603c2a50847ec9e4e12a78a38ec
Teresa Cho <tcho@cca.edu>
2020-06-09 17:21:24 -0700

    gives form inside of AddFishForm.js a class of fish-edit

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 class AddFishForm extends React.Component {
   render() {
     return (
-      <form>
+      <form className='fish-edit'>
         <input name="name" type="text" placeholder="Name" />
         <input name="price" type="text" placeholder="Price" />
         <input name="status" type="text" placeholder="Status" />
