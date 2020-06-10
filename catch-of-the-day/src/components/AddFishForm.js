b84ff02caf967f6e91d6385166ade896e9e873ab
Teresa Cho <tcho@cca.edu>
2020-06-09 17:17:34 -0700

    creates scaffolding for input tags in our fish form

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -4,9 +4,11 @@ class AddFishForm extends React.Component {
   render() {
     return (
       <form>
-        <p>
-          It works y'all!
-        </p>
+        <input name="name" type="text" placeholder="Name" />
+        <input name="price" type="text" placeholder="Price" />
+        <input name="status" type="text" placeholder="Status" />
+        <input name="desc" type="text" placeholder="Desc" />
+        <input name="image" type="text" placeholder="Image" />
       </form>
     );
   }
