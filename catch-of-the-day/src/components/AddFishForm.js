463b44f09404f990abceffb85b5c726ea3884a39
Teresa Cho <tcho@cca.edu>
2020-06-09 17:53:16 -0700

    changes status to dropdown + adds two options

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -6,7 +6,10 @@ class AddFishForm extends React.Component {
       <form className='fish-edit'>
         <input name="name" type="text" placeholder="Name" />
         <input name="price" type="text" placeholder="Price" />
-        <input name="status" type="text" placeholder="Status" />
+        <select name="status">
+          <option value='available'>Fresh!</option>
+          <option value='unavailable'>Sold Out!</option>
+        </select>
         <textarea name="desc" placeholder="Desc" />
         <input name="image" type="text" placeholder="Image" />
         <button type='submit'> + Add Fish</button>
