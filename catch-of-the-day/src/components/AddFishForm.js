b9dcbfa32a2e887219201ffba2902ead690293fe
Teresa Cho <tcho@cca.edu>
2020-06-11 16:32:20 -0700

    moves some input tags onto their own lines for future proofing

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -16,22 +16,32 @@ class AddFishForm extends React.Component {
       status: this.statusRef.current.value,
       desc: this.descRef.current.value,
       image: this.imageRef.current.value,
-    }
+    };
     this.props.addFish(fish);
-
-  }
+  };
   
   render() {
     return (
       <form className='fish-edit' onSubmit={this.createFish}>
         <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
-        <input name="price" ref={this.priceRef} type="text" placeholder="Price" />
+        <input 
+          name="price"
+          ref={this.priceRef} 
+          type="text" 
+          placeholder="Price" 
+        />
         <select name="status" ref={this.statusRef}>
           <option value='available'>Fresh!</option>
           <option value='unavailable'>Sold Out!</option>
         </select>
+
         <textarea name="desc" ref={this.descRef} placeholder="Desc" />
-        <input name="image" ref={this.imageRef} type="text" placeholder="Image" />
+        <input 
+          name="image" 
+          ref={this.imageRef} 
+          type="text" 
+          placeholder="Image"
+        />
         <button type='submit'> + Add Fish</button>
       </form>
     );
