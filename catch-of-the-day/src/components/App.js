63e355e483d24e27cb52b576d52a9a0e9e49ed30
Teresa Cho <tcho@cca.edu>
2020-06-12 16:12:15 -0700

    passes the loadSampleFishes prop into Inventory

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -29,7 +29,10 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory 
+          addFish={this.addFish} 
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
