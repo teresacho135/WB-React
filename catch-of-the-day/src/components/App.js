a029a7a0e18430fcbd743de529f9b0a6589589da
Teresa Cho <tcho@cca.edu>
2020-06-12 16:09:35 -0700

    creates a custom function called loadSampleFishes --> still need to import this function into Inventory

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -18,6 +18,10 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  loadSampleFishes = () => {
+    alert('loading sample fishes')
+  }
+
   render() {
     return (
       <div className='catch-of-the-day'>
