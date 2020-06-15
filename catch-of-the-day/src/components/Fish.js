942228510e704619fce6b1005ffadae19bc41087
Teresa Cho <tcho@cca.edu>
2020-06-14 18:29:53 -0700

    fixes className typo in Fish.js

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -4,9 +4,10 @@ class Fish extends React.Component {
     render() {
       const {image, name, price, description, status} = this.props.details.image;
       return (
-        <li classname='menu-fish'>
+        <li className='menu-fish'>
           <img src={image} alt={name} />
           <h3 className='fish-name'>{name}</h3>
+            <span className='price'>{price}</span>
         </li>
       )
   };
