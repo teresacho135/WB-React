b5755abaa7d0c6248ce0d1110ee6d3b40ffd8097
Teresa Cho <tcho@cca.edu>
2020-06-14 17:34:48 -0700

    creates an img tag of the fish

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Fish extends React.Component {
   render() {
     return (
-      <li classname='single-fish'>🐟</li>
+      <li classname='menu-fish'>
+        <img src={this.props.fish.image} alt={this.props.fish.name} />
+      </li>
     )
   };
 };
