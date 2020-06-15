6986792808bc6e153e92a7bc667da2421f3cce58
Teresa Cho <tcho@cca.edu>
2020-06-14 17:44:46 -0700

    refractors the name into a variable

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,10 +3,11 @@ import React from 'react';
 class Fish extends React.Component {
     render() {
       const image = this.props.details.image;
+      const name = this.props.details.name;
       return (
         <li classname='menu-fish'>
-          <img src={image} alt={this.props.details.name} />
-          <h3 className='fish-name'>{this.props.details.name}</h3>
+          <img src={image} alt={name} />
+          <h3 className='fish-name'>{name}</h3>
         </li>
       )
   };
