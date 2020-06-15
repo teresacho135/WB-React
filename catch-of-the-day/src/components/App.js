32f1b4b7ee69e3c2958d5dd41a15b72d18a6a104
Teresa Cho <tcho@cca.edu>
2020-06-15 16:43:46 -0700

    passes the AddToOrder down to Fish component via props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -40,7 +40,7 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market' />
             <ul className='fishes'>
               {Object.keys(this.state.fishes).map(key => (
-                <Fish key={key} details={this.state.fishes[key]} />
+                <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
               ))}
             </ul>
         </div>
