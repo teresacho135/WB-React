6367085442a04c543c42320c7955854b76732b6c
Teresa Cho <tcho@cca.edu>
2020-06-14 18:27:25 -0700

    destructures more variables that pertain to our pros

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Fish extends React.Component {
     render() {
-      const {image, name} = this.props.details.image;
+      const {image, name, price, description, status} = this.props.details.image;
       return (
         <li classname='menu-fish'>
           <img src={image} alt={name} />
