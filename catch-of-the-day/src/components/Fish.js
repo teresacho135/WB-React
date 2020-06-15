d052554e4fd5aac4142423e82e0af4f5f35fadd6
Teresa Cho <tcho@cca.edu>
2020-06-14 18:30:27 -0700

    also fixes the destrucutre statement

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Fish extends React.Component {
     render() {
-      const {image, name, price, description, status} = this.props.details.image;
+      const {image, name, price, description, status} = this.props.details;
       return (
         <li className='menu-fish'>
           <img src={image} alt={name} />
