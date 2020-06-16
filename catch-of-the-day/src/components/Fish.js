843e77897acfae65f64bd3277e2616d20750ddaa
Teresa Cho <tcho@cca.edu>
2020-06-15 17:28:15 -0700

    fixes error, changes .key to .index

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  }
     render() {
       const {image, name, price, desc, status} = this.props.details;
       const isAvailable = status === 'available';
