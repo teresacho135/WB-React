a5356c60559a5f6c722d1eeac7cd6fd68077e155
Teresa Cho <tcho@cca.edu>
2020-06-16 16:57:13 -0700

    changes the class name to order-wrap and creates h2 tag as well

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Order extends React.Component {
   render() {
-    return <div className="order">Order!!!</div>;
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+      </div>
+    )
   }
 }
 
