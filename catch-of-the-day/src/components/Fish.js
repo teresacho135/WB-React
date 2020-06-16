037e657c6e08fcf2c876172b4ed5a61b67b12216
Teresa Cho <tcho@cca.edu>
2020-06-15 17:33:02 -0700

    changes the handleClick functionality into one line instead

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
-  handleClick = () => {
-    this.props.addToOrder(this.props.index);
-  }
     render() {
       const {image, name, price, desc, status} = this.props.details;
       const isAvailable = status === 'available';
@@ -15,7 +12,7 @@ class Fish extends React.Component {
             <span className='price'>{formatPrice(price)}</span>
           </h3>
           <p>{desc}</p>
-          <button disabled={!isAvailable} onClick={this.handleClick}>
+          <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
             {isAvailable ? 'Add to Cart' : 'Sold Out!'}
           </button>
         
