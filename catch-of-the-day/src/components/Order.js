ecb8bf6c970bdde0c15d20e5fac5056fc2ee2bf2
Teresa Cho <tcho@cca.edu>
2020-06-18 22:57:16 -0700

    creates a variable that will allow us to check if there is that fish available

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -6,6 +6,7 @@ class Order extends React.Component {
     const total = orderIds.reduce((prevTotal, key) => {
       const fish = this.props.fishes[key];
       const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
     });
 
     return (
