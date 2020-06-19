12fb9963714a31fff8cb57bd0554519b960cef03
Teresa Cho <tcho@cca.edu>
2020-06-18 22:59:18 -0700

    creates a conditional that will return the prevTotal --> tally of order before and add the count of the new fish price and if it is not available --> will just return the prevTotal

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -7,6 +7,10 @@ class Order extends React.Component {
       const fish = this.props.fishes[key];
       const count = this.props.order[key];
       const isAvailable = fish && fish.status === 'available';
+      if(isAvailable) {
+        return prevTotal + (count * fish.price)
+      }
+      return prevTotal;
     });
 
     return (
