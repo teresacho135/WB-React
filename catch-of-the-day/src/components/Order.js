0bca9dd7c796e0a40835526b243f74c7b5639f38
Teresa Cho <tcho@cca.edu>
2020-06-20 15:33:17 -0700

    imports the formatPrice function from helpers and moves that down below where the total will be calculated

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
   render() {
@@ -18,7 +19,7 @@ class Order extends React.Component {
         <h2>Order</h2>
         {orderIds}
         <div className='total'>
-          <strong>{total}</strong>
+          <strong>{formatPrice(total)}</strong>
         </div>
       </div>
     )
